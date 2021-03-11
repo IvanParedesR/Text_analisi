7165cb7944b6c977f05c94426c3cfdc7f2591d1d
Ivan Paredes <ivanresendiz25@gmail.com>
2021-03-11 07:42:01 -0600

    local

--- whatsapp.R
+++ whatsapp.R
@@ -106,3 +106,29 @@ miChat %>%
   facet_wrap(~author, ncol = 2, scales = "free_y") +
   scale_x_reordered() +
   ggtitle("Most often used words")
+
+library("stopwords")
+to_remove <- c(stopwords(language = "es"),
+               "que",
+               "de",
+               "en",
+               "el",
+               "la",
+               "no",
+               "android.s.wt")
+
+miChat %>%
+  unnest_tokens(input = text,
+                output = word) %>%
+  filter(!word %in% to_remove) %>%
+  count(author, word, sort = TRUE) %>%
+  group_by(author) %>%
+  top_n(n = 6, n) %>%
+  ggplot(aes(x = reorder_within(word, n, author), y = n, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  ylab("") +
+  xlab("") +
+  coord_flip() +
+  facet_wrap(~author, ncol = 2, scales = "free_y") +
+  scale_x_reordered() +
+  ggtitle("Most often used words")
